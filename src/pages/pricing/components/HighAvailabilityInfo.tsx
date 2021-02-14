941ae5cdd5a1feaeac9d193700605de1035f5bc8
Neil <neil@neils-mbp.lan>
2021-02-15 09:58:04 +1000

    Update high availibility, tidy up.

--- src/pages/pricing/components/HighAvailabilityInfo.tsx
+++ src/pages/pricing/components/HighAvailabilityInfo.tsx
@@ -5,27 +5,24 @@ import Lock from '@material-ui/icons/Lock';
 import styles from './HighAvailability.module.scss';
 
 const HighAvailabilityInfo = (props: any) => {
-  const targetsAtLeast100: boolean = props.targets >= 100;
+  const isHighAvailabilityEnabled: boolean = props.targets >= 100;
+  
   const toggleLabel = () => {
-    if (targetsAtLeast100) {
-      return "Acheivement Unlocked! High availability feature included in plan!"
-    } else {
-      return "High availability feature included with 100 targets or more"
-    }
+    return isHighAvailabilityEnabled
+       ? "Acheivement Unlocked! High availability feature included in plan!"
+       : "High availability feature included with 100 targets or more"
   }
 
   const toggleIcon = () => {
-    if (targetsAtLeast100) {
-      return <LockOpen className={styles.icon} />
-    } else {
-      return <Lock className={styles.icon} />
-    } 
+    return isHighAvailabilityEnabled
+      ? <LockOpen className={styles.icon} />
+      : <Lock className={styles.icon} />
   }
 
   const toggleColor = () => {
-    return targetsAtLeast100 
-    ? "secondary"
-    : "primary"
+    return isHighAvailabilityEnabled
+      ? "secondary"
+      : "primary"
   }
 
   return (
