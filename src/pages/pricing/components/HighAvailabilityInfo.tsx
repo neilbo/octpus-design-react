f4dd2f05acd0e46b9fa9fbe9a6cd8795b2960fc0
Neil <neil@neils-mbp.lan>
2021-02-16 09:43:28 +1000

    Try emotion instead of css module

--- src/pages/pricing/components/HighAvailabilityInfo.tsx
+++ src/pages/pricing/components/HighAvailabilityInfo.tsx
@@ -2,21 +2,21 @@ import * as React from "react";
 import Chip from '@material-ui/core/Chip';
 import LockOpen from '@material-ui/icons/LockOpen';
 import Lock from '@material-ui/icons/Lock';
-import styles from './HighAvailability.module.scss';
+import { css } from '@emotion/css';
 
 const HighAvailabilityInfo = (props: any) => {
   const isHighAvailabilityEnabled: boolean = props.targets >= 100;
-  
+
   const toggleLabel = () => {
     return isHighAvailabilityEnabled
-       ? "Acheivement Unlocked! High availability feature included in plan!"
-       : "High availability feature included with 100 targets or more"
+      ? "Acheivement Unlocked! High availability feature included in plan!"
+      : "High availability feature included with 100 targets or more"
   }
 
   const toggleIcon = () => {
     return isHighAvailabilityEnabled
-      ? <LockOpen className={styles.icon} />
-      : <Lock className={styles.icon} />
+      ? <LockOpen />
+      : <Lock />
   }
 
   const toggleColor = () => {
@@ -27,13 +27,16 @@ const HighAvailabilityInfo = (props: any) => {
 
   return (
     <Chip
-      className={styles.ha_chip}
+      className={css`
+      padding: 24px 16px;
+      color: white;
+      `}
       icon={toggleIcon()}
       color={toggleColor()}
       label={toggleLabel()}
+
     />
   )
-
 };
 
 export default HighAvailabilityInfo;
